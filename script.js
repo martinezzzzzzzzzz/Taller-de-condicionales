b63823652dc83f25ddb56d9116a1c67d9703cdc6
Emanuel <martinezvalencia1200@gmail.com>
2024-08-21 15:45:59 -0500

    Ejercicio 14

--- script.js
+++ script.js
@@ -174,3 +174,17 @@ if (usuario === "admin" && contrasena === "1234") {
 } else {
     console.log("Acceso denegado");
 }
+
+//EJERCICIO 14
+
+let hora = prompt("Ingrese la hora (solo el número de la hora, de 0 a 23): ");
+
+if (hora >= 6 && hora < 12) {
+    console.log("Buenos días");
+} else if (hora >= 12 && hora < 18) {
+    console.log("Buenas tardes");
+} else if ((hora >= 18 && hora < 24) || (hora >= 0 && hora < 6)) {
+    console.log("Buenas noches");
+} else {
+    console.log("Hora no válida");
+}
