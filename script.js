7a1496b3d72fc0284f20f696a5ae7a4b1d5bdaab
Emanuel <martinezvalencia1200@gmail.com>
2024-08-21 15:46:37 -0500

    Ejercicio 15

--- script.js
+++ script.js
@@ -188,3 +188,25 @@ if (hora >= 6 && hora < 12) {
 } else {
     console.log("Hora no válida");
 }
+
+// EJERCICIO 15
+
+let peso = prompt("Ingrese su peso en kilogramos: ");
+let altura = prompt("Ingrese su altura en metros: ");
+
+peso = parseFloat(peso);
+altura = parseFloat(altura);
+
+let imc = peso / (altura * altura);
+
+if (imc < 18.5) {
+    console.log("Bajo peso");
+} else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Peso normal");
+} else if (imc >= 25 && imc <= 29.9) {
+    console.log("Sobrepeso");
+} else if (imc > 30) {
+    console.log("Obesidad");
+} else {
+    console.log("Datos no válidos");
+}
